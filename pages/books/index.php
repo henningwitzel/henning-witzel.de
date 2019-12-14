178698f2104cc9f4161793109ca06f92518aae59
Henning Witzel <privat@henning-witzel.de>
2019-12-14 19:40:24 +0100

    Inital commit of books

--- pages/books/index.php
+++ pages/books/index.php
@@ -7,11 +7,90 @@
       </a>
     </div>
      ###menu###
-    <div class='content'>      
+    <div class='content books'>      
       <h1>Books</h1>
-      <h4 class="grey bottom-large">All my favourite books.</h4>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi blandit cursus risus at ultrices mi tempus. Viverra orci sagittis eu volutpat odio facilisis mauris sit.</p> <p>Vestibulum lorem sed risus ultricies tristique nulla aliquet enim. Morbi tincidunt augue interdum velit euismod in pellentesque. Molestie a iaculis at erat pellentesque adipiscing commodo. Condimentum vitae sapien pellentesque habitant morbi tristique senectus et netus. Felis eget nunc lobortis mattis. Ut tristique et egestas quis ipsum suspendisse ultrices. Vitae sapien pellentesque habitant morbi tristique senectus et. Consectetur adipiscing elit duis tristique. In metus vulputate eu scelerisque felis imperdiet.</p>      
+      <h4 class="grey bottom-large">A selection of some of my favourite books.</h4>
+      <br />      
+      <div class="book">
+        <div class="cover">
+          <a href="https://www.goodreads.com/book/show/28257707-the-subtle-art-of-not-giving-a-f-ck" target="_blank">
+            <image src="https://images-na.ssl-images-amazon.com/images/I/71QKQ9mwV7L.jpg">
+          </a>
+        </div>
+        <div class="details">
+          <h3>The Subtle Art of Not Giving a F*ck</h3>
+          <h5 class="grey bottom-medium">Book by Mark Manson · 2016</h5>
+          <p>In it Manson argues that life's struggles give it meaning, and that the mindless positivity of typical self-help books is neither practical nor helpful.</p>
+          <a href="https://www.goodreads.com/book/show/28257707-the-subtle-art-of-not-giving-a-f-ck" target="_blank">More on Goodreads</a>
+        </div>
+        <div class='clearfix'></div>
     </div>
-    <div class='clearfix'></div>
+    
+    <div class="book">
+        <div class="cover">
+          <a href="https://www.goodreads.com/book/show/18077903-creativity-inc" target="_blank">
+              <image src="https://images-na.ssl-images-amazon.com/images/I/41NR3XoAdxL.jpg">
+            </a>
+          </div>
+        <div class="details">
+          <h3>Creativity, Inc.</h3>
+          <h5 class="grey bottom-medium">Book by by Amy Wallace and Edwin Catmull · 2014</h5>
+          <p>Overcoming the Unseen Forces That Stand in the Way of True Inspiration is a 2014 book, written by Amy Wallace and Edwin Catmull, about managing creativity.</p>
+          <a href="https://www.goodreads.com/book/show/18077903-creativity-inc" target="_blank">More on Goodreads</a>
+        </div>
+        <div class='clearfix'></div>
+    </div>    
+    <div class="book">
+        <div class="cover">
+          <a href="https://www.goodreads.com/book/show/6828896-delivering-happiness" target="_blank">
+            <image src="https://dynamic.indigoimages.ca/books/0446576220.jpg?altimages=false&scaleup=true&width=614&maxheight=614&quality=85&lang=en"></div>
+          </a>
+        <div class="details">
+          <h3>Delivering Happiness</h3>
+          <h5 class="grey bottom-medium">Book by Tony Hsieh · 2010</h5>
+          <p>Delivering Happiness is a book by Zappos CEO Tony Hsieh. It details his life as an entrepreneur, with emphasis on the founding of LinkExchange and Zappos.</p>
+          <a href="https://www.goodreads.com/book/show/6828896-delivering-happiness" target="_blank">More on Goodreads</a>
+        </div>
+        <div class='clearfix'></div>
+    </div>
+    <div class="book">
+        <div class="cover">
+          <a href="https://www.goodreads.com/book/show/23692271-sapiens">
+            <image src="https://images-eu.ssl-images-amazon.com/images/I/41yu2qXhXXL.jpg" target="_blank"></div>
+          </a>
+        <div class="details">
+          <h3>Sapiens</h3>
+          <h5 class="grey bottom-medium">Book by Yuval Noah Harari · 2011</h5>
+          <p>The book surveys the history of humankind from the evolution of archaic human species in the Stone Age up to the twenty-first century, focusing on Homo sapiens.</p>
+          <a href="https://www.goodreads.com/book/show/23692271-sapiens" target="_blank">More on Goodreads</a>
+        </div>
+        <div class='clearfix'></div>
+    </div>
+    <div class="book">
+        <div class="cover">
+          <a href="https://www.goodreads.com/book/show/17316682-remote" target="_blank">
+            <image src="https://basecamp.com/assets/books/remote/remote-cover-aadeb20bf72bc28d49a49d2433e731d36c8a255f9d496880a5224e1ce0006577.png"></div>
+          </a>
+        <div class="details">
+          <h3>Remote</h3>
+          <h5 class="grey bottom-medium">Book by Jason Fried and David Heinemeier Hansson · 2013</h5>
+          <p>In this book, Jason Fried and David Heinemeier Hansson show both employers and employees how they can work together, remotely, from any desk, in any</p>
+          <a href="https://www.goodreads.com/book/show/17316682-remote" target="_blank">More on Goodreads</a>
+        </div>
+        <div class='clearfix'></div>
+    </div>
+    <div class="book">
+        <div class="cover">
+          <a href="https://www.goodreads.com/book/show/38900866-it-doesn-t-have-to-be-crazy-at-work" target="_blank">
+            <image src="https://images-eu.ssl-images-amazon.com/images/I/51nepS2j-UL.jpg"></div>
+          </a>
+        <div class="details">
+          <h3>It Doesn't Have to Be Crazy at Work</h3>
+          <h5 class="grey bottom-medium">Book by Jason Fried and David Heinemeier Hansson · 2018</h5>
+          <p>In this timely manifesto, the authors of the New York Times bestseller Rework broadly reject the prevailing notion that long hours, aggressive hustle, and "whatever it takes" are required to run a successful business today.</p>
+          <a href="https://www.goodreads.com/book/show/38900866-it-doesn-t-have-to-be-crazy-at-work" target="_blank">More on Goodreads</a>
+        </div>
+        <div class='clearfix'></div>
+    </div>
   </div>
 </div>
