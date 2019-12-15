a9d871b86834bd6664500864d664ad329bdb0d6e
Henning Witzel <privat@henning-witzel.de>
2019-12-15 13:32:54 +0100

    Added "Jump to book" link

--- pages/books/index.php
+++ pages/books/index.php
@@ -11,14 +11,14 @@
       <h1>Books</h1>
       <h4 class="grey bottom-large">A selection of some of my favourite books in my bookshelf.</h4>
       <br />      
-      <div class="book">
+      <div id="manson" class="book">
         <div class="cover">
-          <a href="https://www.goodreads.com/book/show/28257707-the-subtle-art-of-not-giving-a-f-ck" target="_blank">
+          <ahref="https://www.goodreads.com/book/show/28257707-the-subtle-art-of-not-giving-a-f-ck" target="_blank">
             <image src="https://images-na.ssl-images-amazon.com/images/I/71QKQ9mwV7L.jpg">
           </a>
         </div>
         <div class="details">
-          <h3>The Subtle Art of Not Giving a F*ck</h3>
+          <h3><a href="#manson">The Subtle Art of Not Giving a F*ck</a></h3>
           <h5 class="grey bottom-medium">Book by Mark Manson · 2016</h5>
           <p>In it Manson argues that life's struggles give it meaning, and that the mindless positivity of typical self-help books is neither practical nor helpful.</p>
           <a href="https://www.goodreads.com/book/show/28257707-the-subtle-art-of-not-giving-a-f-ck" target="_blank">More on Goodreads</a>
@@ -26,92 +26,92 @@
         <div class='clearfix'></div>
     </div>
     
-    <div class="book">
+    <div id="creativity" class="book">
         <div class="cover">
           <a href="https://www.goodreads.com/book/show/18077903-creativity-inc" target="_blank">
               <image src="https://images-na.ssl-images-amazon.com/images/I/41NR3XoAdxL.jpg">
             </a>
           </div>
         <div class="details">
-          <h3>Creativity, Inc.</h3>
+          <h3><a href="#creativity">Creativity, Inc</a></h3>
           <h5 class="grey bottom-medium">Book by by Amy Wallace and Edwin Catmull · 2014</h5>
           <p>Overcoming the Unseen Forces That Stand in the Way of True Inspiration is a 2014 book, written by Amy Wallace and Edwin Catmull, about managing creativity.</p>
           <a href="https://www.goodreads.com/book/show/18077903-creativity-inc" target="_blank">More on Goodreads</a>
         </div>
         <div class='clearfix'></div>
     </div>    
-    <div class="book">
+    <div id="happiness" class="book">
         <div class="cover">
           <a href="https://www.goodreads.com/book/show/6828896-delivering-happiness" target="_blank">
             <image src="https://dynamic.indigoimages.ca/books/0446576220.jpg?altimages=false&scaleup=true&width=614&maxheight=614&quality=85&lang=en"></div>
           </a>
         <div class="details">
-          <h3>Delivering Happiness</h3>
+          <h3><a href="#happiness">Delivering Happiness</a></h3>
           <h5 class="grey bottom-medium">Book by Tony Hsieh · 2010</h5>
           <p>Delivering Happiness is a book by Zappos CEO Tony Hsieh. It details his life as an entrepreneur, with emphasis on the founding of LinkExchange and Zappos.</p>
           <a href="https://www.goodreads.com/book/show/6828896-delivering-happiness" target="_blank">More on Goodreads</a>
         </div>
         <div class='clearfix'></div>
     </div>
-    <div class="book">
+    <div id="sapines" class="book">
         <div class="cover">
           <a href="https://www.goodreads.com/book/show/23692271-sapiens">
             <image src="https://images-eu.ssl-images-amazon.com/images/I/41yu2qXhXXL.jpg" target="_blank"></div>
           </a>
         <div class="details">
-          <h3>Sapiens</h3>
+          <h3><a href="#sapines">Sapiens</a></h3>
           <h5 class="grey bottom-medium">Book by Yuval Noah Harari · 2011</h5>
           <p>The book surveys the history of humankind from the evolution of archaic human species in the Stone Age up to the twenty-first century, focusing on Homo sapiens.</p>
           <a href="https://www.goodreads.com/book/show/23692271-sapiens" target="_blank">More on Goodreads</a>
         </div>
         <div class='clearfix'></div>
     </div>
-    <div class="book">
+    <div id="remote" class="book">
         <div class="cover">
           <a href="https://www.goodreads.com/book/show/17316682-remote" target="_blank">
             <image src="https://basecamp.com/assets/books/remote/remote-cover-aadeb20bf72bc28d49a49d2433e731d36c8a255f9d496880a5224e1ce0006577.png"></div>
           </a>
         <div class="details">
-          <h3>Remote</h3>
+          <h3><a href="#remote">Remote</a></h3>
           <h5 class="grey bottom-medium">Book by Jason Fried and David Heinemeier Hansson · 2013</h5>
           <p>In this book, Jason Fried and David Heinemeier Hansson show both employers and employees how they can work together, remotely, from any desk, in any</p>
           <a href="https://www.goodreads.com/book/show/17316682-remote" target="_blank">More on Goodreads</a>
         </div>
         <div class='clearfix'></div>
     </div>
-    <div class="book">
+    <div id="crazy" class="book">
         <div class="cover">
           <a href="https://www.goodreads.com/book/show/38900866-it-doesn-t-have-to-be-crazy-at-work" target="_blank">
             <image src="https://images-eu.ssl-images-amazon.com/images/I/51nepS2j-UL.jpg"></div>
           </a>
         <div class="details">
-          <h3>It Doesn't Have to Be Crazy at Work</h3>
+          <h3><a href="#crazy">It Doesn't Have to Be Crazy at Work</a></h3>
           <h5 class="grey bottom-medium">Book by Jason Fried and David Heinemeier Hansson · 2018</h5>
           <p>In this timely manifesto, the authors of the New York Times bestseller Rework broadly reject the prevailing notion that long hours, aggressive hustle, and "whatever it takes" are required to run a successful business today.</p>
           <a href="https://www.goodreads.com/book/show/38900866-it-doesn-t-have-to-be-crazy-at-work" target="_blank">More on Goodreads</a>
         </div>
         <div class='clearfix'></div>
     </div>
-    <div class="book">
+    <div id="writing" class="book">
         <div class="cover">
           <a href="https://www.goodreads.com/book/show/53343.On_Writing_Well" target="_blank">
             <image src="https://images-na.ssl-images-amazon.com/images/I/610AIYRtaDL.jpg"></div>
           </a>
         <div class="details">
-          <h3>On Writing Well</h3>
+          <h3><a href="#writing">On Writing Well</a></h3>
           <h5 class="grey bottom-medium">Book by William Zinsser · 1976</h5>
           <p>On Writing Well has been praised for its sound advice, its clarity and the warmth of its style. It is a book for everybody who wants to learn how to write or who needs to do some writing to get through the day, as almost everybody does in the age of e-mail and the Internet.</p>
           <a href="https://www.goodreads.com/book/show/53343.On_Writing_Well" target="_blank">More on Goodreads</a>
         </div>
         <div class='clearfix'></div>
     </div>
-    <div class="book">
+    <div id="shapeup" class="book">
         <div class="cover">
           <a href="https://basecamp.com/shapeup" target="_blank">
             <image src="https://miro.medium.com/max/500/1*77mgyQKo9DHom4hqXGc3ww.jpeg"></div>
           </a>
         <div class="details">
-          <h3>Shape Up</h3>
+          <h3><a href="#shapeup">Shape Up</a></h3>
           <h5 class="grey bottom-medium">Book by Ryan Singer · 2019</h5>
           <p>This is an online web book about how Basecamp does their work. This book is a guide to how we do product development at Basecamp. It’s also a toolbox full of techniques that you can apply in your own way to your own process.</p>
           <a href="https://basecamp.com/shapeup" target="_blank">Start reading!</a>
