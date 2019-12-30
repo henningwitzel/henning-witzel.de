33a8761ee5c35223001266c2174312bf649c2ae1
Henning Witzel <privat@henning-witzel.de>
2019-12-30 12:14:56 +0100

    Fixing spelling mistakes

--- pages/blog/basecamp/index.php
+++ pages/blog/basecamp/index.php
@@ -2,7 +2,7 @@
 </head>
   <body basepath='###basepath###'></body>
   <div id='clearbutton'></div>
-</html>  
+</html>
 <div class='image' 
 data_image_1280='###basepath###img/header/basecamp-header-1280.jpg' 
 data_image_1440='###basepath###img/header/basecamp-header-1440.jpg' 
@@ -24,14 +24,16 @@ data_image_800='###basepath###img/header/basecamp-header-800.jpg' id='header'></
       <p>After reading "Shape Up" I tried out the <i>30-day</i> trial version with my girlfriend. We liked how easy it was to manage projects and don't lose oversight. After the period ended, we discussed if we should extend the trial or spend the $99 every month. Not using it was out of the picture.</p>
       <p>Luckily this problem went away since I discovered the free version. At that point, I struggled a little with my memories since I was thinking: <i>"Why haven't I seen this in the first place? Was it even there? I can't find any announcement."</i> </p>
       <p>Now with the Basecamp Personal version available, I wanted to use it with my UX design team. I signed up, and after a couple of weeks, I loved it and wanted more – the full version for everybody at the company. Helping others to grow is one of my goals inside the UX team – and Basecamp can help.</p>
-      <h3 class="top-extra-large bottom-medium">Knowledge got lost over time</h3>
-      <p>For almost 5 years, I'm working at <a href="###basepath###/about_me#newstore">NewStore</a>. I experienced that a lot of information and valuable knowledge got lost over time. All these tools we used for communication that are supposed to help didn't make it better. It doesn't matter if its email, Jira, Confluence, Slack, Trello, Drive, or Asana. You're recent article about <a href="https://basecamp.com/guides/group-chat-problems" target="_blank">group chat</a> resonated well with my experience.</p>
+      <h3 class="top-extra-large bottom-small">Knowledge got lost over time</h3>
+      <p>For almost 5 years, I'm working at <a href="###basepath###/about_me#newstore">NewStore</a>. I experienced that a lot of information and valuable knowledge got lost over time. All these tools we used for communication that are supposed to help didn't make it better. It doesn't matter if its email, Jira, Confluence, Slack, Trello, Drive, or Asana. The recent article about <a href="https://basecamp.com/guides/group-chat-problems" target="_blank">group chat problems</a> resonated well with my experience.</p>
       <p>Since the beginning, we were driving for a remote working culture. We have three offices in Germany, two in the US and people working from their home offices. Over time we learned how important communication is, but we are still struggling to get it right.</p>
-      <h3 class="top-extra-large bottom-medium">Convincing the CPO</h2>
-      <p>By giving Basecamp a try,I realized that saying there is a tool that could solve some of our problems didn't work – <i>Oh jeez, another tool!?</i></p>
+      <h3 class="top-extra-large bottom-small">Convincing the CPO</h2>
+      <p>By giving Basecamp a try, I realized that saying there is a tool that could solve some of our problems didn't work – <i>Oh jeez, another tool!?</i></p>
       <p>I decided for a bottom-up approach. I used it by myself and explained how it improved my work. By sharing concepts and meeting notes via the public link feature, I caught attention. </p>
       <p>I looked for people in the company that were interested in asynchronous communication and pointed out Basecamp. It turned out that a lot now it from their past and spoke very positive about it. The majority used Basecamp 2 a couple of years ago and didn't know about Basecamp 3.</p>
       <p>My next step was to convince the CPO by giving it a try in the product team for a certain amount of time to see the adoption. By applying the learnings I made using the personal version, I created a document. My goal was to show how Basecamp could work <a href="https://twitter.com/Henning_Witzel/status/1193139821130240000" target="_blank">for the whole company</a>.</p>
+      <p><blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">Trying to convince people at my company to use <a href="https://twitter.com/basecamp?ref_src=twsrc%5Etfw">@basecamp</a> by putting real examples together that show how we could streamline our communication and potentially replace some tools or extend them to be more useful. <a href="https://t.co/4kn2fzJ1V9">pic.twitter.com/4kn2fzJ1V9</a></p>&mdash; Henning Witzel (@Henning_Witzel) <a href="https://twitter.com/Henning_Witzel/status/1193139821130240000?ref_src=twsrc%5Etfw">November 9, 2019</a></blockquote>
+      <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></p>
       <p>Showing the CPO this and walking through my UX project I could convince her giving it a try for six months. Then we talked about pricing.</p>
       <p>She couldn't believe that it will only cost us $99 a month with unlimited users. Even our IT team doubt it until the last second before signing up for Basecamp Business – perfect name by the way. I'm a big fan of the current pricing model and it was one of the many reasons for getting to the final go!</p>
       <p>Recently we also signed up for <a href="https://miro.com/" target="_blank">Miro</a> to collaborate remotely in front of a digital whiteboard. The tool works great, but for every user that contributes, we have to pay extra and that sucks and reduces the adoption inside the engineering team.</p>
