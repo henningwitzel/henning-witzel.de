92f66405ee98a2c292a2fc3d3a54f09d30472afa
Henning Witzel <privat@henning-witzel.de>
2020-01-20 18:48:33 +0100

    Added new podcast - The Mom Test

--- pages/podcasts/index.php
+++ pages/podcasts/index.php
@@ -38,10 +38,30 @@ Huế</a> · Vietnam 2019</div></div>
         <h4 class="grey bottom-large">A selection of some of my favourite podcasts I recently listened to.</h4>
         <br />
         <div class="podcast">
+          <div class="left"><a href="https://brianrhea.com" target="_blank"><img src="https://static.pocketcasts.com/discover/images/400/5790c200-4f52-0137-f266-1d245fc5f9cf.jpg" /></a></div>
+          <div class="right">
+            <h3>Rob Fitzpatrick: Conducting Better Customer Interviews with "The Mom Test"</h3>
+            <h5 class="grey bottom-small">55 minutes · September 11th, 2019</h5>
+            <p>Rob is the author of The Mom Test: How to talk to customers & learn if your business is a good idea when everyone is lying to you. He and I talk about common mistakes people make when conducting customers interviews and how to avoid them.</p>
+            <audio preload="none" controls>  
+              <source src="https://cdn.transistor.fm/file/transistor/m/shows/1793/f36307cd27db90ed4a4e4f37be5150dc.mp3" type="audio/mpeg" preload="none">
+              Your browser does not support the audio element.
+            </audio> 
+            <div class="top-small link">
+              <div class="logo-left">
+                <img width="20px" src="https://pbs.twimg.com/profile_images/1062998714455744512/rDpTZ7Rk_400x400.jpg"/>
+              </div>
+              <div class="link-right"><a href="https://pca.st/tszy9g8t" target="_blank">Listen on Pocket Cast </a>
+            </div>
+          </div>
+        </div>
+        <div class='clearfix'></div>
+        </div>
+        <div class="podcast">
           <div class="left"><a href="http://www.peterattiamd.com/" target="_blank"><img src="https://static.pocketcasts.com/discover/images/400/dc8b3d00-56bc-0136-fa7c-0fe84b59566d.jpg" /></a></div>
           <div class="right">
             <h3>Jason Fried: Optimizing efficiency and work-life balance</h3>
-            <h5 class="grey bottom-small">October 7th, 2019</h5>
+            <h5 class="grey bottom-small">2 hours and 24 minutes · October 7th, 2019</h5>
             <p>I really enjoyed the 2,5 hours long conversation between Peter Attia and Jason Fried, co-founder of Basecamp. They spoke about achieving business success in a modern world, aspects of work-life balance, and what it means to “work hard”. If you are interested in Jason, you will learn something about his backstory and the creation of Basecamp.</p>
             <audio preload="none" controls>  
               <source src="http://traffic.libsyn.com/peterattiadrive/EP.74_-_Jason_Fried_FINAL.mp3?dest-id=713489" type="audio/mpeg" preload="none">
@@ -61,7 +81,7 @@ Huế</a> · Vietnam 2019</div></div>
           <div class="left"><a href="https://changelog.com/" target="_blank"><img src="https://static.pocketcasts.com/discover/images/400/70d13d50-9efe-0130-1b90-723c91aeae46.jpg" /></a></div>
           <div class="right">
             <h3>Shaping, betting, and building</h3>
-            <h5 class="grey bottom-small">16th August 2019</h5>
+            <h5 class="grey bottom-small">1 hour and 28 minutes · 16th August 2019</h5>
             <p>After reading the book <a href="###basepath###/books#shapeup">Shape Up</a> from Ryan Singer, I was curious to learn more about it. Luckily there are several podcasts around where he was interviewed. After listening to almost all of them, I found this one most comprehensive.</p>
             <audio preload="none" controls>  
               <source src="https://cdn.changelog.com/uploads/podcast/357/the-changelog-357.mp3" type="audio/mpeg" preload="none">
@@ -81,7 +101,7 @@ Huế</a> · Vietnam 2019</div></div>
           <div class="left"><a href="https://soundcloud.com/larochefm" target="_blank"><img src="https://static.pocketcasts.com/discover/images/400/7445db60-aab7-0135-9e5b-5bb073f92b78.jpg" /></a></div>
           <dic class="right">
             <h3>How consistent writing makes you a better designer</h3>
-            <h5 class="grey bottom-small">October 21st, 2019</h5>
+            <h5 class="grey bottom-small">22 minutes · October 21st, 2019</h5>
             <p>If you are a designer and want to become better in what you do, then start writing. In this episode, Eugeniu is talking about how consistent writing will help. To be honest, I never was much of a writer. When this website was only about my photography, I was not too fond of writing a text to each post. Even writing my Bachelor's and Master's thesis wasn't always fun. My native language is German, and it took me some time to became more fluent in English writing. Like often in life, things change. I realized how much writing helped me structure my thoughts, communicate more explicit, and help others to grow.</p>
             <audio preload="none" controls>  
               <source src="http://feeds.soundcloud.com/stream/699462082-larochefm-ep48.mp3" type="audio/mpeg" >
