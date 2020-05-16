7d9a53e4f5042bb6ee22705e8fb5d043211d97b0
Henning Witzel <privat@henning-witzel.de>
2020-05-16 20:51:55 +0200

    Add new blog article about LICEcap

---
+++ pages/blog/gif/index.php
@@ -0,0 +1,60 @@
+<title>Create GIFs to show a user flow · Henning Witzel</title>
+<meta property="og:title" content="Create GIFs to show a user flow · Henning Witzel" />
+<meta name="twitter:title" content="Create GIFs to show a user flow · Henning Witzel" />
+
+<meta name="description" content="As a designer, there are often moments where I designed a user flow and created a prototype to indicate a specific interaction I had in mind. The next step is usually to share it with a product owner, developer, or put it into a document to describe the flow." />
+<meta property="og:description" content="As a designer, there are often moments where I designed a user flow and created a prototype to indicate a specific interaction I had in mind. The next step is usually to share it with a product owner, developer, or put it into a document to describe the flow." />
+<meta name="twitter:description" content="As a designer, there are often moments where I designed a user flow and created a prototype to indicate a specific interaction I had in mind. The next step is usually to share it with a product owner, developer, or put it into a document to describe the flow." />
+
+<meta property="og:image" content="###basepath###img/header/gif-header-1280.jpg" />
+<meta name="twitter:image" content="###basepath###img/header/gif-header-1280.jpg" />
+
+<meta name="twitter:card" content="summary" />
+<meta name="twitter:site" content="@henning_witzel" />
+</head>
+  <body basepath='###basepath###'></body>
+  <div id='clearbutton'></div>
+</html>
+<div class='image' 
+data_image_1280='###basepath###img/header/gif-header-1280.jpg' 
+data_image_1440='###basepath###img/header/gif-header-1440.jpg' 
+data_image_1920='###basepath###img/header/gif-header-1920.jpg' 
+data_image_800='###basepath###img/header/gif-header-800.jpg' id='header'></div>
+<div class='text' id='background'>
+  <div id='wrapper'>
+    <div id='logo'>
+      <a href='###basepath###'>
+        <div id='link'></div>
+      </a>
+    </div>
+     ###menu###
+    <div class='content'>          
+      <h1>Create GIFs to show a user flow</h1>
+      <h4 class="grey bottom-large">May 15th, 2020 · 2 min read</h4>
+      <p>As a designer, there are often moments where I designed a user flow and created a prototype to indicate a specific interaction I had in mind. The next step is usually to share it with a product owner, developer, or put it into a document to describe the flow.</p>
+
+      <p>The easiest way for me to do this is to capture my desktop and create a GIF or video.</p>
+
+      <p>As an example you can see below how Grammarly helped me to correct and improve this text. The GIF shows how the interaction works, and I do not depend on you to click a play button – it will catch your attention.</p>
+
+      <a class="image-link" href="###basepath###pages/blog/gif/images/grammarly-correction.gif"><img class="bottom-large" src="###basepath###pages/blog/gif/images/grammarly-correction.gif" /></a>
+
+      <p>For many years I've been using <a href="https://www.cockos.com/licecap/" target="_blank">LICEcap</a>, a simple tool to make animated screen captures for OS X and Windows. For my needs, it does what it is supposed to do. You can capture any area of your desktop and save it directly as a GIF file. That's it, nothing more, nothing less, and it comes for free.</p>
+
+      <p>To record, you simply position and adjust the frame to what you want to capture on the screen. In the settings, you can control the frames per second and show mouse clicks if you wish. They just recently provided an update to make it work on the latest Mac OSX version.</p>
+
+      <a class="image-link" href="###basepath###pages/blog/gif/images/licecap.png"><img class="bottom-large" src="###basepath###pages/blog/gif/images/licecap.png" /></a>
+      
+      <h3 class="top-extra-large bottom-small">More complex flows</h3>
+
+      <p>If I need to show more complex flows and express my thoughts, I will create a video by recording my screen with the Quick Time Player and use the microphone to capture my voice. As an example, I recently shared a concept inside our product team to communicate the user experience.</p>
+      <a class="image-link" href="###basepath###pages/blog/gif/images/videos.png"><img class="bottom-large" src="###basepath###pages/blog/gif/images/videos.png" /></a>
+      <p>Everybody can watch it at their own time and provide feedback. I can speak to the video and explain my thoughts behind – easy todo without much of an effort. It felt better to publish it like this than having a meeting where everybody is silently watching me presenting. If there are open questions, we can still get together and discuss it. </p>
+
+      <p>I hope this helps you to improve communicating your user flows and designs.</p>
+      
+      <p>Stay safe and healthy 🙂</p>
+    </div>
+    <div class='clearfix'></div>
+  </div>
+</div>
