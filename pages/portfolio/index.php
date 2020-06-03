6bcc3cad496c11c9c427062507450dc9a46b9ee4
Henning Witzel <privat@henning-witzel.de>
2020-06-03 13:46:22 +0200

    Add Portfolio (1st version)

---
+++ pages/portfolio/index.php
@@ -0,0 +1,155 @@
+
+<title>Portfolio · Henning Witzel</title>
+<meta property="og:title" content="Portfolio · Henning Witzel" />
+<meta name="twitter:title" content="Portfolio · Henning Witzel" />
+
+<meta name="description" content="Lorem ipsum dolor sit amet, cum ne saepe malorum dissentiet." />
+<meta property="og:description" content="Lorem ipsum dolor sit amet, cum ne saepe malorum dissentiet..">
+<meta name="twitter:description" content="Lorem ipsum dolor sit amet, cum ne saepe malorum dissentiet.." />
+
+<meta property="og:image" content="###basepath###img/header/###page###-header-1280.jpg" />
+<meta name="twitter:image" content="###basepath###img/header/###page###-header-1280.jpg" />
+
+<meta name="twitter:card" content="summary" />
+<meta name="twitter:site" content="@henning_witzel" />
+
+</head>
+  <body basepath='###basepath###'></body>
+  <div id='clearbutton'></div>
+</html>
+<div class='image' 
+data_image_1280='###basepath###img/header/portfolio-header-1280.jpg' 
+data_image_1440='###basepath###img/header/portfolio-header-1440.jpg' 
+data_image_1920='###basepath###img/header/portfolio-header-1920.jpg'
+data_image_800='###basepath###img/header/portfolio-header-800.jpg' 
+id='header'></div>
+<div class='text' id='background'>
+  <div id='wrapper'>
+    <div id='logo'>
+      <a href='###basepath###'>
+        <div id='link'></div>
+      </a>
+    </div>
+     ###menu###
+    <div class='content portfolio'>      
+        <h1>Portfolio</h1>
+        <h4 class="grey bottom-large">Some products I designed and launched.</h4>        
+        <p class="in-progress">🚧 This site is still work in progress and there will be regular updates in the next weeks 🚧</br><i>Last update on Jun 2nd, 2020</i></p>
+        <p>Below you can find a selection of software products and designs I've worked on over that past nine years. There are no concept designs; everything you can see is implemented and went live. </p>
+        
+        <p class="bottom-large">While I was hiring designers I found many out there that publish designs that may look nice but never have been developed or solve an actual problem. For me, this is important. Every time I design something, it's just a mockup until we are getting real with it. Many factors need to be incorporated to ensure feasibility. Collaboration with developers and user feedback is essential for good design. In the end, you need to know if you solved the problem and ideally made their life easier.</p>
+        <!--<h3 class="bottom-small">NewStore – Bring Joy Back to Retail</h3>-->
+        <!--<p class="bottom-large"> The first product I want to show you is from <a href="###basepath###/about_me#newstore">my time at NewStore</a>. The Associate App is in use by thousands of store associates every day at retailers like Decathlon, Burton, UNTUCKit, or Outdoor Voices.</p>-->
+        
+        <div class="app">
+          <div class="intro">            
+            <h2>Associate App</h2>
+            <h4 class="grey bottom-medium">iOS App · 2017 - Present</h4>        
+            <p>The first product I want to show you is from <a href="###basepath###/about_me#newstore">my time at NewStore</a>. For the last three years I'm the lead designer for the Associate App, a mobile POS that makes it simple for store associates to adopt and serve customers. We allow them to access inventory and customer data across the brand to streamline checkout and create customer engagement.</p>
+            <p>The Associate App is in use by thousands of store associates every day at retailers like Decathlon, Burton, UNTUCKit, or Outdoor Voices.</p>
+            <p>For the portfolio I put together a set of problems that needed to be solved where good user experience was critical for daily use.</p>
+          </div>          
+          
+          <div class="white-bg">
+            <div class="left"><img src="###basepath###pages/portfolio/images/mixed-checkout.png" /></div>
+            <div class="right">
+              <h3 class="bottom-small top-small">Mixed Cart</h3>
+              <p>Enable associates to sell items to a customer that are available in the store and online in one transaction.</p>
+                <a class="learn-more" href="#">Learn more</a>
+            </div>
+            <div class='clearfix'></div>
+            <div class="phone"><img src="###basepath###pages/portfolio/images/mixed-checkout-phone.png" /></div>
+          </div>
+          <div class="grey-bg">
+            <div class="left">
+              <h3 class="bottom-small top-small">Contactless Checkout</h3>              
+              <p>Remove the friction of using a physical payment device and make it easy for a customer to share it's contact info for easy returns.</p>
+              <a class="learn-more" href="#">Learn more</a>
+            </div>
+            <div class="right"><img src="###basepath###pages/portfolio/images/qr-code-payment.png" /></div>
+            <div class='clearfix'></div>
+            <div class="phone"><img src="###basepath###pages/portfolio/images/qr-code-payment.png" /></div>
+          </div>
+          
+          <div class="white-bg">
+            <div class="left"><img src="###basepath###pages/portfolio/images/login.png" /></div>
+            <div class="right">
+              <h3 class="bottom-small top-small">Quick Login</h3>
+                <p>Lorem ipsum dolor sit amet, cum ne saepe malorum dissentiet, et eum iuvaret pertinax invenire.</p>
+                <a class="learn-more" href="#">Learn more</a>
+            </div>
+            <div class='clearfix'></div>
+            <div class="phone"><img src="###basepath###pages/portfolio/images/login.png" /></div>
+          </div>
+          <div class="grey-bg">
+            <div class="left">
+              <h3 class="bottom-small top-small">Cash Management</h3>
+              <p>Lorem ipsum dolor sit amet, cum ne saepe malorum dissentiet, et eum iuvaret pertinax invenire.</p>
+              <a class="learn-more" href="#">Learn more</a>
+            </div>
+            <div class="right"><img src="###basepath###pages/portfolio/images/cash-management.png" /></div>
+            <div class='clearfix'></div>
+            <div class="phone"><img src="###basepath###pages/portfolio/images/cash-management-phone.png" /></div>
+          </div>
+        </div>
+        <div class="app">
+          <div class="intro">            
+            <h2>Omnichannel Manager</h2>
+            <h4 class="grey bottom-medium">Web App · 2015 - 2017</h4>        
+            <p>The second product is also from <a href="###basepath###/about_me#newstore">my time at NewStore</a>. For the first two years I was the lead designer for the Ominchannel Manager, a backoffice web app that provides a complete view of customers, orders, inventory, revenue, and store performance across all channels.</p>            
+          </div>
+          <div class="white-bg">
+            <div class="left"><img src="###basepath###pages/portfolio/images/nom.png" /></div>
+            <div class="right">
+              <h3 class="bottom-small top-small">Manage Retail Operations</h3>
+                <p>Lorem ipsum dolor sit amet, cum ne saepe malorum dissentiet, et eum iuvaret pertinax invenire.</p>
+                <a class="learn-more" href="#">Learn more</a>
+            </div>                                
+            <div class='clearfix'></div>
+            <div class="phone"><img src="###basepath###pages/portfolio/images/nom.png" /></div>
+          </div>
+          </div>                
+        <div class="app">
+          <div class="intro">            
+            <h2>signum+</h2>
+            <h4 class="grey bottom-medium">Web App · 2013 - 2014</h4>        
+            <p>The third product I want to show you is from <a href="###basepath###/about_me#zebresel">my time at Zebresel</a>. Lorem ipsum dolor sit amet, cum ne saepe malorum dissentiet, et eum iuvaret pertinax invenire.</p>            
+          </div>
+          <div class="white-bg">
+            <div class="left"><img src="###basepath###pages/portfolio/images/signum.png" /></div>
+            <div class="right">
+              <h3 class="bottom-small top-small">Digital Signage</h3>
+                <p>Lorem ipsum dolor sit amet, cum ne saepe malorum dissentiet, et eum iuvaret pertinax invenire.</p>
+                <a class="learn-more" href="#">Learn more</a>
+            </div>                     
+            <div class='clearfix'></div>
+            <div class="phone"><img src="###basepath###pages/portfolio/images/signum.png" /></div>
+          </div>
+          </div>
+          <div class="app">
+          <div class="intro">            
+            <h2>Beamerlasershow</h2>
+            <h4 class="grey bottom-medium">Web App · 2013</h4>        
+            <p>The fourth product is also from <a href="###basepath###/about_me#zebresel">my time at Zebresel</a>. Lorem ipsum dolor sit amet, cum ne saepe malorum dissentiet, et eum iuvaret pertinax invenire.</p>            
+          </div>          
+          <div class="grey-bg">
+            <div class="left">
+              <h3 class="bottom-small top-small">Artificial fog meets projector</h3>
+              <p>Lorem ipsum dolor sit amet, cum ne saepe malorum dissentiet, et eum iuvaret pertinax invenire.</p>
+              <a class="learn-more" href="#">Learn more</a>
+            </div>
+            <div class="right"><img src="###basepath###pages/portfolio/images/bls.png" /></div>
+            <div class='clearfix'></div>
+            <div class="phone"><img src="###basepath###pages/portfolio/images/bls.png" /></div>
+          </div>
+          </div>               
+        <div class="product">
+          <h2>Webdesign & Development</h2>
+          <h4 class="grey bottom-medium">2011 - 2014</h4>    
+          <p>Lorem ipsum dolor sit amet, cum ne saepe malorum dissentiet, et eum iuvaret pertinax invenire.</p>
+          <a class="learn-more" href="#">Learn more</a>
+          </div>
+        <div class="website"></div>
+        
+  </div>
+</div>
