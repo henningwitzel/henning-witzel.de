98f52233b6804df5e1ed0338b7f33906d0ddab19
Henning Witzel <privat@henning-witzel.de>
2020-06-03 16:05:17 +0200

    Smaller UI changes on portfolio

--- pages/portfolio/index.php
+++ pages/portfolio/index.php
@@ -33,14 +33,10 @@ id='header'></div>
      ###menu###
     <div class='content portfolio'>      
         <h1>Portfolio</h1>
-        <h4 class="grey bottom-large">Some products I designed and launched.</h4>        
-        <p class="in-progress">🚧 This site is still work in progress and there will be regular updates in the next weeks 🚧</br><i>Last update on Jun 2nd, 2020</i></p>
-        <p>Below you can find a selection of software products and designs I've worked on over that past nine years. There are no concept designs; everything you can see is implemented and went live. </p>
-        
-        <p class="bottom-large">While I was hiring designers I found many out there that publish designs that may look nice but never have been developed or solve an actual problem. For me, this is important. Every time I design something, it's just a mockup until we are getting real with it. Many factors need to be incorporated to ensure feasibility. Collaboration with developers and user feedback is essential for good design. In the end, you need to know if you solved the problem and ideally made their life easier.</p>
-        <!--<h3 class="bottom-small">NewStore – Bring Joy Back to Retail</h3>-->
-        <!--<p class="bottom-large"> The first product I want to show you is from <a href="###basepath###/about_me#newstore">my time at NewStore</a>. The Associate App is in use by thousands of store associates every day at retailers like Decathlon, Burton, UNTUCKit, or Outdoor Voices.</p>-->
-        
+        <h4 class="grey bottom-large">Some products I designed and launched 🚀</h4>        
+        <p class="in-progress">🚧 This site is still work in progress and there will be regular updates in the next weeks 🚧</br><i>Last update on Jun 3rd, 2020</i></p>
+        <p>Below you can find a selection of software products and designs I've worked on over that past nine years. There are no concept designs; everything you can see is implemented and went live. </p>      
+        <p class="bottom-large">While I was hiring designers I found many out there that publish designs that may look nice but never have been developed or solve an actual problem. For me, this is important. Every time I design something, it's just a mockup until we are getting real with it. Many factors need to be incorporated to ensure feasibility. Collaboration with developers and user feedback is essential for good design. In the end, you need to know if you solved the problem and ideally made their life easier.</p>        
         <div class="app">
           <div class="intro">            
             <h2>Associate App</h2>
@@ -48,8 +44,7 @@ id='header'></div>
             <p>The first product I want to show you is from <a href="###basepath###/about_me#newstore">my time at NewStore</a>. For the last three years I'm the lead designer for the Associate App, a mobile POS that makes it simple for store associates to adopt and serve customers. We allow them to access inventory and customer data across the brand to streamline checkout and create customer engagement.</p>
             <p>The Associate App is in use by thousands of store associates every day at retailers like Decathlon, Burton, UNTUCKit, or Outdoor Voices.</p>
             <p>For the portfolio I put together a set of problems that needed to be solved where good user experience was critical for daily use.</p>
-          </div>          
-          
+          </div>                    
           <div class="white-bg">
             <div class="left"><img src="###basepath###pages/portfolio/images/mixed-checkout.png" /></div>
             <div class="right">
@@ -69,8 +64,7 @@ id='header'></div>
             <div class="right"><img src="###basepath###pages/portfolio/images/qr-code-payment.png" /></div>
             <div class='clearfix'></div>
             <div class="phone"><img src="###basepath###pages/portfolio/images/qr-code-payment.png" /></div>
-          </div>
-          
+          </div>          
           <div class="white-bg">
             <div class="left"><img src="###basepath###pages/portfolio/images/login.png" /></div>
             <div class="right">
@@ -113,12 +107,12 @@ id='header'></div>
           <div class="intro">            
             <h2>signum+</h2>
             <h4 class="grey bottom-medium">Web App · 2013 - 2014</h4>        
-            <p>The third product I want to show you is from <a href="###basepath###/about_me#zebresel">my time at Zebresel</a>. Lorem ipsum dolor sit amet, cum ne saepe malorum dissentiet, et eum iuvaret pertinax invenire.</p>            
+            <p>The third product I want to show you is from <a href="###basepath###/about_me#zebresel">my time at Zebresel</a>.  It allows you to manage content for every kind of screen. With the usage of social media, we enabled our customers to engage with their guests during events. Together with a display solution partner, we were able to show off the power of the product at the Rock am Ring festival in Germany 2014.</p>            
           </div>
           <div class="white-bg">
             <div class="left"><img src="###basepath###pages/portfolio/images/signum.png" /></div>
             <div class="right">
-              <h3 class="bottom-small top-small">Digital Signage</h3>
+              <h3 class="bottom-small top-small">Digital Signage & Social Media </h3>
                 <p>Lorem ipsum dolor sit amet, cum ne saepe malorum dissentiet, et eum iuvaret pertinax invenire.</p>
                 <a class="learn-more" href="#">Learn more</a>
             </div>                     
@@ -129,27 +123,26 @@ id='header'></div>
           <div class="app">
           <div class="intro">            
             <h2>Beamerlasershow</h2>
-            <h4 class="grey bottom-medium">Web App · 2013</h4>        
-            <p>The fourth product is also from <a href="###basepath###/about_me#zebresel">my time at Zebresel</a>. Lorem ipsum dolor sit amet, cum ne saepe malorum dissentiet, et eum iuvaret pertinax invenire.</p>            
-          </div>          
-          <div class="grey-bg">
-            <div class="left">
-              <h3 class="bottom-small top-small">Artificial fog meets projector</h3>
-              <p>Lorem ipsum dolor sit amet, cum ne saepe malorum dissentiet, et eum iuvaret pertinax invenire.</p>
-              <a class="learn-more" href="#">Learn more</a>
-            </div>
-            <div class="right"><img src="###basepath###pages/portfolio/images/bls.png" /></div>
+            <h4 class="grey bottom-medium">iOS App · 2013</h4>        
+            <p>The fourth product is also from <a href="###basepath###/about_me#zebresel">my time at Zebresel</a>. The app allows you to create a light-show and fascinate your guests wherever you want. We combined a video projector, artificial fog, and 2D animations with an iOS device. The app for iPhone and iPad allows you to create playlists with a big set of predefined animations. By allowing to touch and draw animations on the screen, the live mix mode unfolded your creativity.</p>            
+          </div>   
+          <div class="white-bg">
+            <div class="left"><img src="###basepath###pages/portfolio/images/bls.png" /></div>
+            <div class="right">
+              <h3 class="bottom-small top-small">Playlists & Live Drawing</h3>
+                <p>Lorem ipsum dolor sit amet, cum ne saepe malorum dissentiet, et eum iuvaret pertinax invenire.</p>
+                <a class="learn-more" href="#">Learn more</a>
+            </div>                     
             <div class='clearfix'></div>
             <div class="phone"><img src="###basepath###pages/portfolio/images/bls.png" /></div>
-          </div>
+          </div>                 
           </div>               
-        <div class="product">
+        <!--<div class="product">
           <h2>Webdesign & Development</h2>
           <h4 class="grey bottom-medium">2011 - 2014</h4>    
           <p>Lorem ipsum dolor sit amet, cum ne saepe malorum dissentiet, et eum iuvaret pertinax invenire.</p>
           <a class="learn-more" href="#">Learn more</a>
           </div>
-        <div class="website"></div>
-        
+        <div class="website"></div> -->       
   </div>
 </div>
