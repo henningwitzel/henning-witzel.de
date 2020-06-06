faeea4ab3a5f1cc96254188ae94fee41b7eb1100
Henning Witzel <privat@henning-witzel.de>
2020-06-06 19:52:06 +0200

    Improve portfolio overview

--- pages/portfolio/index.php
+++ pages/portfolio/index.php
@@ -33,7 +33,7 @@ id='header'></div>
      ###menu###
     <div class='content portfolio'>      
         <h1>Portfolio</h1>
-        <h4 class="grey bottom-large">Some products I designed and launched 🚀</h4>                
+        <h4 class="grey bottom-large">Some products I designed and launched<span class="rocket">🚀</span></h4>                
         <p>Below you can find a selection of software products and designs I've worked on over that past nine years. There are no concept designs; everything you can see is implemented and went live. </p>
         <p class="bottom-large">While I was hiring designers I found many out there that publish designs that may look nice but never have been developed or solve an actual problem. For me, this is important. Every time I design something, it's just a mockup until we are getting real with it. Many factors need to be incorporated to ensure feasibility. Collaboration with developers and user feedback is essential for good design. In the end, you need to know if you solved the problem and ideally made their life easier.</p>
         <p class="in-progress bottom-large">🚧 This site is still work in progress and there will be regular updates in the next weeks 🚧</br><i>Last update on Jun 3rd, 2020</i></p>
@@ -41,16 +41,17 @@ id='header'></div>
           <div class="intro">            
             <h2>Associate App</h2>
             <h4 class="grey bottom-medium">iOS App · 2017 - Present</h4>        
-            <p>The first product I want to show you is from <a href="###basepath###/about_me#newstore">my time at NewStore</a>. For the last three years I'm the lead designer for the Associate App, a mobile POS that makes it simple for store associates to adopt and serve customers. We allow them to access inventory and customer data across the brand to streamline checkout and create customer engagement.</p>
+            <p>The first product I want to show you is from my time at <a href="###basepath###/about_me#newstore">NewStore</a>. For the last three years I'm the lead designer for the Associate App, a mobile POS that makes it simple for store associates to adopt and serve customers. We allow them to access inventory and customer data across the brand to streamline checkout and create customer engagement.</p>
             <p>The Associate App is in use by thousands of store associates every day at retailers like Decathlon, Burton, UNTUCKit, or Outdoor Voices.</p>
-            <p>For the portfolio I put together a set of problems that needed to be solved where good user experience was critical for daily use.</p>
+            <p class="bottom-medium">For the portfolio I put together a set of problems that needed to be solved where good user experience was critical for daily use.</p>
+            <a class="learn-more" href="###basepath######page###/associate_app">Learn more</a>
           </div>                    
           <div class="white-bg">
             <div class="left"><img src="###basepath###pages/portfolio/images/mixed-checkout.png" /></div>
             <div class="right">
               <h3 class="bottom-small top-small">Mixed Cart</h3>
               <p>Enable associates to sell items to a customer that are available in the store and online in one transaction.</p>
-                <a class="learn-more" href="###basepath######page###/associate_app#cart">Learn more</a>
+                <!--<a class="learn-more" href="###basepath######page###/associate_app#cart">Learn more</a>-->
             </div>
             <div class='clearfix'></div>
             <div class="phone"><img src="###basepath###pages/portfolio/images/mixed-checkout-phone.png" /></div>
@@ -59,7 +60,7 @@ id='header'></div>
             <div class="left">
               <h3 class="bottom-small top-small">Contactless Checkout</h3>              
               <p>Remove the friction of using a physical payment device and make it easy for a customer to share it's contact info for easy returns.</p>
-              <a class="learn-more" href="###basepath######page###/associate_app#checkout">Learn more</a>
+              <!--<a class="learn-more" href="###basepath######page###/associate_app#checkout">Learn more</a>-->
             </div>
             <div class="right"><img src="###basepath###pages/portfolio/images/qr-code-payment.png" /></div>
             <div class='clearfix'></div>
@@ -70,7 +71,7 @@ id='header'></div>
             <div class="right">
               <h3 class="bottom-small top-small">Quick Login</h3>
                 <p>Lorem ipsum dolor sit amet, cum ne saepe malorum dissentiet, et eum iuvaret pertinax invenire.</p>
-                <a class="learn-more" href="###basepath######page###/associate_app#login">Learn more</a>
+                <!--<a class="learn-more" href="###basepath######page###/associate_app#login">Learn more</a>-->
             </div>
             <div class='clearfix'></div>
             <div class="phone"><img src="###basepath###pages/portfolio/images/login.png" /></div>
@@ -79,7 +80,7 @@ id='header'></div>
             <div class="left">
               <h3 class="bottom-small top-small">Cash Management</h3>
               <p>Lorem ipsum dolor sit amet, cum ne saepe malorum dissentiet, et eum iuvaret pertinax invenire.</p>
-              <a class="learn-more" href="###basepath######page###/associate#cash">Learn more</a>
+              <!--<a class="learn-more" href="###basepath######page###/associate#cash">Learn more</a>-->
             </div>
             <div class="right"><img src="###basepath###pages/portfolio/images/cash-management.png" /></div>
             <div class='clearfix'></div>
@@ -90,52 +91,34 @@ id='header'></div>
           <div class="intro">            
             <h2>Omnichannel Manager</h2>
             <h4 class="grey bottom-medium">Web App · 2015 - 2017</h4>        
-            <p>The second product is also from <a href="###basepath###/about_me#newstore">my time at NewStore</a>. For the first two years I was the lead designer for the Ominchannel Manager, a backoffice web app that provides a complete view of customers, orders, inventory, revenue, and store performance across all channels.</p>            
+            <p class="bottom-medium">The second product is also from my time at <a href="###basepath###/about_me#newstore">NewStore</a>. For the first two years I was the lead designer for the Ominchannel Manager, a backoffice web app that provides a complete view of customers, orders, inventory, revenue, and store performance across all channels.</p>
+            <a class="learn-more" href="###basepath######page###/nom">Learn more</a>
           </div>
-          <div class="white-bg">
-            <div class="left"><img src="###basepath###pages/portfolio/images/nom.png" /></div>
-            <div class="right">
-              <h3 class="bottom-small top-small">Manage Retail Operations</h3>
-                <p>Lorem ipsum dolor sit amet, cum ne saepe malorum dissentiet, et eum iuvaret pertinax invenire.</p>
-                <a class="learn-more" href="###basepath######page###/nom">Learn more</a>
-            </div>                                
-            <div class='clearfix'></div>
-            <div class="phone"><img src="###basepath###pages/portfolio/images/nom.png" /></div>
+          <div class="product-full">
+            <img src="###basepath###pages/portfolio/images/nom.png" />
           </div>
           </div>                
         <div class="app">
           <div class="intro">            
             <h2>signum+</h2>
             <h4 class="grey bottom-medium">Web App · 2013 - 2014</h4>        
-            <p>The third product I want to show you is from <a href="###basepath###/about_me#zebresel">my time at Zebresel</a>.  It allows you to manage content for every kind of screen. With the usage of social media, we enabled our customers to engage with their guests during events. Together with a display solution partner, we were able to show off the power of the product at the Rock am Ring festival in Germany 2014.</p>            
+            <p class="bottom-medium">The third product I want to show you is from my time at <a href="###basepath###/about_me#zebresel">Zebresel</a>.  It allows you to manage content for every kind of screen. With the usage of social media, we enabled our customers to engage with their guests during events. Together with a display solution partner, we were able to show off the power of the product at the Rock am Ring festival in Germany 2014.</p>
+            <a class="learn-more" href="###basepath######page###/signum">Learn more</a>
           </div>
-          <div class="white-bg">
-            <div class="left"><img src="###basepath###pages/portfolio/images/signum.png" /></div>
-            <div class="right">
-              <h3 class="bottom-small top-small">Digital Signage & Social Media </h3>
-                <p>Lorem ipsum dolor sit amet, cum ne saepe malorum dissentiet, et eum iuvaret pertinax invenire.</p>
-                <a class="learn-more" href="###basepath######page###/signum">Learn more</a>
-            </div>                     
-            <div class='clearfix'></div>
-            <div class="phone"><img src="###basepath###pages/portfolio/images/signum.png" /></div>
-          </div>
+          <div class="product-full">
+            <img src="###basepath###pages/portfolio/images/signum.png" />
+         </div>
           </div>
           <div class="app">
           <div class="intro">            
             <h2>Beamerlasershow</h2>
             <h4 class="grey bottom-medium">iOS App · 2013</h4>        
-            <p>The fourth product is also from <a href="###basepath###/about_me#zebresel">my time at Zebresel</a>. The app allows you to create a light-show and fascinate your guests wherever you want. We combined a video projector, artificial fog, and 2D animations with an iOS device. The app for iPhone and iPad allows you to create playlists with a big set of predefined animations. By allowing to touch and draw animations on the screen, the live mix mode unfolded your creativity.</p>            
+            <p>The fourth product is also from my time at <a href="###basepath###/about_me#zebresel">Zebresel</a>. The app allows you to create a light-show and fascinate your guests wherever you want. We combined a video projector, artificial fog, and 2D animations with an iOS device. The app for iPhone and iPad allows you to create playlists with a big set of predefined animations. By allowing to touch and draw animations on the screen, the live mix mode unfolded your creativity.</p>
+            <a class="learn-more" href="###basepath######page###/beamerlasershow">Learn more</a>
           </div>   
-          <div class="white-bg">
-            <div class="left"><img src="###basepath###pages/portfolio/images/bls.png" /></div>
-            <div class="right">
-              <h3 class="bottom-small top-small">Playlists & Live Drawing</h3>
-                <p>Lorem ipsum dolor sit amet, cum ne saepe malorum dissentiet, et eum iuvaret pertinax invenire.</p>
-                <a class="learn-more" href="###basepath######page###/beamerlasershow">Learn more</a>
-            </div>                     
-            <div class='clearfix'></div>
-            <div class="phone"><img src="###basepath###pages/portfolio/images/bls.png" /></div>
-          </div>                 
+          <div class="product-full">
+            <img src="###basepath###pages/portfolio/images/bls.png" />
+          </div>    
           </div>               
         <!--<div class="product">
           <h2>Webdesign & Development</h2>
